eef8be5a14ca5905536ca87986472f94fd662e0d
dreamer-1829 <nnodenexus@gmail.com>
2025-11-05 11:14:24 +0530

    Add program to check if a number is positive, negative, or zero

---
+++ programiz questions/example010.py
@@ -0,0 +1,8 @@
+#Python Program to Check if a Number is Positive, Negative or 0
+number=(int(input("enter the number: ")))
+if number > 0:
+    print("the number is positive")
+elif number < 0:
+    print("the is negative")
+else:
+    print("the number is xero")
