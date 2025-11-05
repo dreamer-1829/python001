b1c74fd54256a751add8a11ae74422bd596158e0
dreamer-1829 <nnodenexus@gmail.com>
2025-11-05 11:09:15 +0530

    Add temperature conversion program from Celsius to Fahrenheit

---
+++ programiz questions/example09.py
@@ -0,0 +1,6 @@
+#In the program below, we take a temperature in degree Celsius and convert it into degree Fahrenheit. They are related by the formula:
+
+#fahrenheit = celsius * 1.8 + 32
+celsius = float(input("Enter temperature in degree Celsius: "))
+fahrenheit = celsius * 1.8 + 32
+print(f"{celsius} degree Celsius is equal to {fahrenheit} degree Fahrenheit")
